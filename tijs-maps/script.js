ceb5a19643a42cab8691ccc7aee6ce520b790ee9
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2021-08-12 17:15:05 +0200

    add user location

--- tijs-maps/script.js
+++ tijs-maps/script.js
@@ -26,3 +26,19 @@ var map_123 = setupMap();
 
 //makeList(listOfDataNames);
 
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+function showPosition(position) {
+
+    geojson_features = L.featureGroup().addTo(map_123);
+
+    L.marker([position.coords.latitude,position.coords.longitude]).addTo(geojson_features);
+
+    layercontrol.addOverlay(geojson_features,'user location')
+}
